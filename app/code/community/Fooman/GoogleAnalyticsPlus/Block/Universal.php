12fe76c781c199e475afd04e5f0d185cb2fb77ed
Lucas van Staden <lucas@dhemdia.com.au>
2014-09-16 10:47:05 +0800

    Attempt to fix whitespace/tab issue

--- app/code/community/Fooman/GoogleAnalyticsPlus/Block/Universal.php
+++ app/code/community/Fooman/GoogleAnalyticsPlus/Block/Universal.php
@@ -10,119 +10,109 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-class Fooman_GoogleAnalyticsPlus_Block_Universal extends Fooman_GoogleAnalyticsPlus_Block_GaConversion
-{
-    protected function _construct()
-    {
-        parent::_construct();
-        $this->setTemplate('fooman/googleanalyticsplus/universal.phtml');
-    }
+class Fooman_GoogleAnalyticsPlus_Block_Universal extends Fooman_GoogleAnalyticsPlus_Block_GaConversion {
 
-    /**
-     * should we include the universal snippet
-     *
-     * @return bool
-     */
-    public function shouldInclude()
-    {
-        if (parent::shouldInclude()) {
-            return $this->isUniversalEnabled() && (bool)$this->getUniversalAccount();
-        } else {
-            return false;
-        }
-    }
+	protected function _construct() {
+		parent::_construct();
+		$this->setTemplate('fooman/googleanalyticsplus/universal.phtml');
+	}
 
-    /**
-     * are we using universal
-     *
-     * @return bool
-     */
-    public function isUniversalEnabled()
-    {
-        return Mage::getStoreConfigFlag('google/analyticsplus_universal/enabled');
-    }
+	/**
+	 * should we include the universal snippet
+	 *
+	 * @return bool
+	 */
+	public function shouldInclude() {
+		if (parent::shouldInclude()) {
+			return $this->isUniversalEnabled() && (bool) $this->getUniversalAccount();
+		} else {
+			return false;
+		}
+	}
 
-    /**
-     * get universal snippet from settings
-     *
-     * @deprecated since 0.14.0
-     * @return string
-     */
-    public function getUniversalSnippet()
-    {
-        return '';
-    }
+	/**
+	 * are we using universal
+	 *
+	 * @return bool
+	 */
+	public function isUniversalEnabled() {
+		return Mage::getStoreConfigFlag('google/analyticsplus_universal/enabled');
+	}
 
-    /**
-     * get Google Analytics account id
-     *
-     * @return mixed
-     */
-    public function getUniversalAccount()
-    {
-        return Mage::getStoreConfig('google/analyticsplus_universal/accountnumber');
-    }
+	/**
+	 * get universal snippet from settings
+	 *
+	 * @deprecated since 0.14.0
+	 * @return string
+	 */
+	public function getUniversalSnippet() {
+		return '';
+	}
 
-    /**
-     * @return bool
-     */
-    public function getUniversalAnonymise()
-    {
-        return Mage::getStoreConfigFlag('google/analyticsplus_universal/anonymise');
-    }
+	/**
+	 * get Google Analytics account id
+	 *
+	 * @return mixed
+	 */
+	public function getUniversalAccount() {
+		return Mage::getStoreConfig('google/analyticsplus_universal/accountnumber');
+	}
 
-    /**
+	/**
+	 * @return bool
+	 */
+	public function getUniversalAnonymise() {
+		return Mage::getStoreConfigFlag('google/analyticsplus_universal/anonymise');
+	}
+
+	/**
 	 * Build any params that is passed on create of analytics object
 	 * 
-     * @return string
-     */
-    public function getUniversalParams()
-    {
-        $params = array();
+	 * @return string
+	 */
+	public function getUniversalParams() {
+		$params = array();
 		if (Mage::getStoreConfig('google/analyticsplus_universal/domainname')) {
-            $params['cookieDomain'] = Mage::getStoreConfig('google/analyticsplus_universal/domainname');
-        }
+			$params['cookieDomain'] = Mage::getStoreConfig('google/analyticsplus_universal/domainname');
+		}
 		if ($this->canUseUniversalUserTracking()) {
-            $params['userId'] = $this->getCustomerId();
-        }
-		if(count($params) == 0) {
+			$params['userId'] = $this->getCustomerId();
+		}
+		if (count($params) == 0) {
 			return "'auto'";
 		}
 		return json_encode($params);
-    }
+	}
 
-    /**
-     * Enable the Display Advertising Features.
-     *
-     * @return bool
-     */
-    public function getUniversalDisplayAdvertising()
-    {
-        return Mage::getStoreConfigFlag('google/analyticsplus_universal/display_advertising');
-    }
-
-    /**
-     * Return cookiename for Display Advertising
-     *
-     * @return String
-     */
-    public function getUniversalDisplayAdvertisingCookieName()
-    {
-        return Mage::getStoreConfig('google/analyticsplus_universal/display_advertising_cookiename');
-    }
-	
 	/**
-     * Is universal user tracking available.
+	 * Enable the Display Advertising Features.
+	 *
+	 * @return bool
+	 */
+	public function getUniversalDisplayAdvertising() {
+		return Mage::getStoreConfigFlag('google/analyticsplus_universal/display_advertising');
+	}
+
+	/**
+	 * Return cookiename for Display Advertising
+	 *
+	 * @return String
+	 */
+	public function getUniversalDisplayAdvertisingCookieName() {
+		return Mage::getStoreConfig('google/analyticsplus_universal/display_advertising_cookiename');
+	}
+
+	/**
+	 * Is universal user tracking available.
 	 * Must be enabled in admin and a user must be logged in
 	 * TODO: Use persistent login data!
-     *
-     * @return bool
-     */
-    public function canUseUniversalUserTracking()
-    {
-        return (Mage::getStoreConfigFlag('google/analyticsplus_universal/userid_tracking') && Mage::getSingleton('customer/session')->isLoggedIn()) ? true : false;
-    }
-	
+	 *
+	 * @return bool
+	 */
+	public function canUseUniversalUserTracking() {
+		return (Mage::getStoreConfigFlag('google/analyticsplus_universal/userid_tracking') && Mage::getSingleton('customer/session')->isLoggedIn()) ? true : false;
+	}
+
 	/**
 	 * Get the current logged in customer id
 	 *
@@ -134,6 +124,5 @@ class Fooman_GoogleAnalyticsPlus_Block_Universal extends Fooman_GoogleAnalyticsP
 		}
 		return false;
 	}
-
 
 }
